cfcd02c0f7f86bf3ef270226da6b69b0194ed26f
mbhuet <mbhuet@gmail.com>
2017-11-29 20:39:12 -0500

    one more file

--- Assets/Scripts/BehaviorSettings.cs
+++ Assets/Scripts/BehaviorSettings.cs
@@ -7,30 +7,7 @@ using System.Reflection;
 using System.Linq;
 using Vexe.Runtime.Types;
 
-[System.Serializable]
-public class BehaviorInfo
-{
-	[ShowSerializableType(typeof(SteeringBehavior)), OnChanged("Foo")]
-	public SerializableType behaviorType;
 
-    [fSlider(0, 10f)]
-    public float weight = 1;
-    public float effectiveRadius = 10;
-
-	[Hide]
-	bool showAnimCurve = false;
-	[VisibleWhen("showAnimCurve")]
-	public ClipAnimationInfoCurve curve;
-
-	void Foo(SerializableType behaviorType){
-		string behaviorTypeName = behaviorType.ToString ();
-		if (behaviorTypeName == "CohesionBehavior") {
-			showAnimCurve = true;
-		} else
-			showAnimCurve = false;
-	}
-    
-}
 
 public class BehaviorSettings : BaseScriptableObject {
 
@@ -38,8 +15,60 @@ public class BehaviorSettings : BaseScriptableObject {
     public float maxSpeed = 2;    // Maximum speed
 
 	[Display(Seq.PerItemRemove)]
-    public BehaviorInfo[] behaviors = { };
-    
+
+    public AlignmentBehavior alignmment = new AlignmentBehavior();
+    public CohesionBehavior cohesion = new CohesionBehavior();
+    public SeparationBehavior separation = new SeparationBehavior();
+    public AvoidanceBehavior avoidance = new AvoidanceBehavior();
+
+    private void Awake()
+    {
+        GetActiveBehaviors();
+    }
+    private void OnEnable()
+    {
+        GetActiveBehaviors();
+    }
+
+    private List<SteeringBehavior> m_allBehaviors;
+    public List<SteeringBehavior> allBehaviors {
+        get {
+            if (m_allBehaviors == null) { GetAllBehaviors(); }
+            return m_allBehaviors;  }
+    }
+
+    private List<SteeringBehavior> m_activeBehaviors;
+    public List<SteeringBehavior> activeBehaviors
+    { get{
+            if (m_activeBehaviors == null){ GetActiveBehaviors();}
+            return m_activeBehaviors;   }
+    }
+
+    private void GetAllBehaviors()
+    {
+        m_allBehaviors = new List<SteeringBehavior>();
+        FieldInfo[] fields = this.GetType().GetFields();
+        foreach (FieldInfo field in fields)
+        {
+            //Debug.Log(field.Name + " " + field.FieldType.ToString());
+            if (field.FieldType.IsSubclassOf(typeof(SteeringBehavior))){
+                //Debug.Log("is behavior");
+                SteeringBehavior behavior = (SteeringBehavior)field.GetValue(this);
+                behavior.OnActiveStatusChange += GetActiveBehaviors;
+                m_allBehaviors.Add(behavior);
+            }
+        }
+    }
+
+    private void GetActiveBehaviors()
+    {
+        m_activeBehaviors = new List<SteeringBehavior>();
+        foreach (SteeringBehavior behavior in allBehaviors)
+        {
+            if (behavior.isActive)
+                m_activeBehaviors.Add(behavior);
+        }
+    }
 
     [MenuItem("Assets/Create/Behavior Settings")]
     public static void CreateMyAsset()
