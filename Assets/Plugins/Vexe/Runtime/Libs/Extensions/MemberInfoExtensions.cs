8f02e23bb5bf679a3d15014e7104ced0d293c8d3
mbhuet <mbhuet@gmail.com>
2019-09-17 20:39:17 -0500

    removed outdated vexe

--- Assets/Plugins/Vexe/Runtime/Libs/Extensions/MemberInfoExtensions.cs
+++ Assets/Plugins/Vexe/Runtime/Libs/Extensions/MemberInfoExtensions.cs
@@ -7,10 +7,6 @@ namespace Vexe.Runtime.Extensions
 {
     public static class MemberInfoExtensions
     {
-        public static bool IsDefined(this MemberInfo minfo, Type type)
-        {
-            return minfo.IsDefined(type, false);
-        }
 
         /// <summary>
         /// Returns true if the attribute whose type is specified by the generic argument is defined on this member
@@ -57,34 +53,7 @@ namespace Vexe.Runtime.Extensions
             return GetDataType(memberInfo, null);
         }
 
-        /// <summary>
-        /// Returns the first found custom attribute of type T on this member
-        /// Returns null if none was found
-        /// </summary>
-        public static T GetCustomAttribute<T>(this MemberInfo member, bool inherit) where T : Attribute
-        {
-            var all = GetCustomAttributes<T>(member, inherit).ToArray();
-            return all.IsNullOrEmpty() ? null : all[0];
-        }
 
-        /// <summary>
-        /// Returns the first found non-inherited custom attribute of type T on this member
-        /// Returns null if none was found
-        /// </summary>
-        public static T GetCustomAttribute<T>(this MemberInfo member) where T : Attribute
-        {
-            return GetCustomAttribute<T>(member, false);
-        }
-
-        public static IEnumerable<T> GetCustomAttributes<T>(this MemberInfo member) where T : Attribute
-        {
-            return GetCustomAttributes<T>(member, false);
-        }
-
-        public static IEnumerable<T> GetCustomAttributes<T>(this MemberInfo member, bool inherit) where T : Attribute
-        {
-            return member.GetCustomAttributes(typeof(T), inherit).Cast<T>();
-        }
 
         public static Attribute[] GetAttributes(this MemberInfo member)
         {
