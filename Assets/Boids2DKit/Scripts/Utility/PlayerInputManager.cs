0c7c29dd489d2fc1e74761f6b75c1b24228e01c9
mbhuet <mbhuet@gmail.com>
2019-09-22 21:28:22 -0500

    Re-organization

--- Assets/Boids2DKit/Scripts/Utility/PlayerInputManager.cs
+++ Assets/Boids2DKit/Scripts/Utility/PlayerInputManager.cs
@@ -5,12 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class PlayerInputManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Return))
         {
