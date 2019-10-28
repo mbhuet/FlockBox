058b3017744bbd76ae8fe2f8549bb1aea72d1b87
mbhuet <mbhuet@gmail.com>
2019-10-28 17:01:11 -0500

    added camera scripts

---
+++ Assets/Editor/Camera/SceneCameraControlEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SceneCamControl))]
+public class SceneCamControlEditor : Editor
+{
+    private System.DateTime last = System.DateTime.Now;
+    private double rot;
+    private void OnSceneGUI()
+    {
+        //SceneView.lastActiveSceneView.LookAt(Vector3.zero, Quaternion.Euler(45, System.DateTime.Now.Second * 10 % 360, 0));
+        var delta = (System.DateTime.Now - last).TotalSeconds;
+        last = System.DateTime.Now;
+        rot += delta;
+        SceneView.lastActiveSceneView.pivot = Vector3.zero;
+        SceneView.lastActiveSceneView.rotation = Quaternion.Euler(45, (float)rot *10, 0);
+        SceneView.RepaintAll();
+
+    }
+}
---
+++ Assets/SceneCamControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SceneCamControl : MonoBehaviour
+{
+    public bool move;
+    public Transform focusTarget;
+    public float focusRotation;
+    public float zoomSpeed;
+
+    // Update is called once per frame
+    [ExecuteInEditMode]
+    void Update()
+    {
+
+        if (!move) return;
+        Camera cam = Camera.current;
+        //SceneView.lastActiveSceneView.pivot = focusTarget.position;
+        //SceneView.lastActiveSceneView.rotation = 
+
+
+        if (!cam) return;
+        if (focusTarget)
+        {
+            cam.transform.RotateAround(focusTarget.position, Vector3.up, focusRotation * Time.unscaledDeltaTime);
+        }
+        cam.transform.Translate(cam.transform.forward * zoomSpeed * Time.unscaledDeltaTime);
+
+
+    }
+}
