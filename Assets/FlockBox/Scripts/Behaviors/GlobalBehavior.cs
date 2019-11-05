f3fadc307f628b9659c9850dc48fa03246f3c48e
mbhuet <mbhuet@gmail.com>
2019-11-05 11:09:06 -0600

    added follow leader behavior

---
+++ Assets/FlockBox/Scripts/Behaviors/GlobalBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CloudFine
+{
+    public abstract class GlobalBehavior : SteeringBehavior
+    {
+        public override void AddPerception(SurroundingsContainer surroundings)
+        {
+            base.AddPerception(surroundings);
+            foreach (string tag in filterTags)
+            {
+                if (!surroundings.globalSearchTags.Contains(tag))
+                {
+                    surroundings.globalSearchTags.Add(tag);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/FlockBox/Scripts/Behaviors/LeaderFollowBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CloudFine
+{
+    public class LeaderFollowBehavior : GlobalBehavior
+    {
+        public override void GetSteeringBehaviorVector(out Vector3 steer, SteeringAgent mine, SurroundingsContainer surroundings)
+        {
+            List<Agent> leaders = GetFilteredAgents(surroundings, this);
+
+            if (leaders.Count == 0)
+            {
+                steer = Vector3.zero;
+                return;
+            }
+            mine.GetSeekVector(out steer, leaders[0].Position);
+        }
+    }
+}
