9f457b93a39a4b971192633ff0a2d3ba993ebb54
mbhuet <mbhuet@gmail.com>
2020-01-05 16:44:39 -0600

    rename fov to fieldofview, more descriptive

--- Assets/FlockBox/Scripts/Behaviors/RadialSteeringBehavior.cs
+++ Assets/FlockBox/Scripts/Behaviors/RadialSteeringBehavior.cs
@@ -1,21 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace CloudFine
 {
     public abstract class RadialSteeringBehavior : SteeringBehavior
     {
         public float effectiveRadius = 10;
-        [Range(0f,360f)]
-        public float fov = 360;
+        [Range(0f, 360f), FormerlySerializedAs("fov")]
+        public float fieldOfView = 360;
 
         protected bool WithinEffectiveRadius(SteeringAgent mine, Agent other)
         {
             if (mine == other) return false;
             return (
                 Vector3.SqrMagnitude(mine.Position - other.Position) < effectiveRadius * effectiveRadius //inside radius
-                && Vector3.Angle(mine.Forward, other.Position - mine.Position) <= fov); // inside fov
+                && Vector3.Angle(mine.Forward, other.Position - mine.Position) <= fieldOfView); // inside fov
         }
 
         public override void AddPerception(SurroundingsContainer surroundings)
