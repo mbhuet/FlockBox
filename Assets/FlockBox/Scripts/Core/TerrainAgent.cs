1062a132e585822a83cfd47dd15e2ef87742f884
mbhuet <mbhuet@gmail.com>
2020-01-29 08:44:42 -0600

    added terrainboids behavior

--- Assets/FlockBox/Scripts/Core/TerrainAgent.cs
+++ Assets/FlockBox/Scripts/Core/TerrainAgent.cs
@@ -9,7 +9,7 @@ namespace CloudFine
         public LayerMask terrainLayerMask;
         private Ray terrainRay;
         private RaycastHit terrainHit;
-
+        public float raycastRange = 1000f;
         
         protected override void UpdateVelocity()
         {
@@ -17,10 +17,12 @@ namespace CloudFine
             //project onto terrain, get new velocity
 
             RaycastHit currentPostHit;
-            Physics.Raycast(Position + Vector3.up * 500f, Vector3.down * 1000f, out currentPostHit, 1000, terrainLayerMask);
+            RaycastToTerrain(Position, out currentPostHit);
+            //Physics.Raycast(Position + Vector3.up * 500f, Vector3.down * 1000f, out currentPostHit, 1000, terrainLayerMask);
 
             RaycastHit goalPosHit;
-            Physics.Raycast((Position + Velocity) + Vector3.up * 500f, Vector3.down * 1000f, out goalPosHit, 1000, terrainLayerMask);
+            RaycastToTerrain(Position + Velocity, out goalPosHit);
+            //Physics.Raycast((Position + Velocity) + Vector3.up * 500f, Vector3.down * 1000f, out goalPosHit, 1000, terrainLayerMask);
 
 
             
@@ -38,13 +40,19 @@ namespace CloudFine
             terrainRay.origin = (Position) + Vector3.up * 500f;
             terrainRay.direction = Vector3.down;
             //Debug.DrawRay(terrainRay.origin, terrainRay.direction * 1000, Color.cyan, 10);
-            if (Physics.Raycast(terrainRay, out terrainHit, 1000, terrainLayerMask))
+
+            if (RaycastToTerrain(Position, out terrainHit))
+                //Physics.Raycast(terrainRay, out terrainHit, 1000, terrainLayerMask))
             {
                 Position = terrainHit.point + Vector3.up * shape.radius;
             }
 
         }
 
+        private bool RaycastToTerrain(Vector3 origin, out RaycastHit hit)
+        {
+            return Physics.Raycast(origin + Vector3.up * raycastRange * .5f, Vector3.down, out hit, raycastRange, terrainLayerMask);
+        }
 
     }
 }
