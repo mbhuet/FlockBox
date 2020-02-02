9caef730a41c34979fc995a6b858320d3eab80a6
mbhuet <mbhuet@gmail.com>
2020-02-02 16:07:38 -0600

    followleader behavior uses clear zones

--- Assets/FlockBox/Scripts/Behaviors/LeaderFollowBehavior.cs
+++ Assets/FlockBox/Scripts/Behaviors/LeaderFollowBehavior.cs
@@ -8,6 +8,11 @@ namespace CloudFine
     {
         public float followDistance = 10;
         public float stoppingRadius = 10;
+        public float clearAheadDistance = 30;
+        public float clearAheadRadius = 10;
+
+        private Vector3 pointOnLeaderPath_cached;
+
         public override void GetSteeringBehaviorVector(out Vector3 steer, SteeringAgent mine, SurroundingsContainer surroundings)
         {
             List<Agent> leaders = GetFilteredAgents(surroundings, this);
@@ -19,6 +24,22 @@ namespace CloudFine
             }
 
             Agent closestLeader = SeekBehavior.ClosestPursuableTarget(leaders, mine);
+
+            //check to see if we should clear the way in front of the leader
+            float scalar = Vector3.Dot(mine.Position - closestLeader.Position, closestLeader.Forward.normalized);
+            if (scalar > 0 && scalar < clearAheadDistance)//we are somewhere in front of the leader, potentially in the clear zone ahead of it.
+            {
+                pointOnLeaderPath_cached = closestLeader.Position + closestLeader.Forward.normalized * scalar;
+                float insideClearZone = (pointOnLeaderPath_cached - mine.Position).sqrMagnitude / (clearAheadRadius * clearAheadRadius); //0-1 is inside zone, <1 is outside
+                if (insideClearZone<=1)
+                {
+                    steer = (mine.Position - pointOnLeaderPath_cached).normalized * (1f - insideClearZone) * mine.activeSettings.maxForce;
+                    return;
+                }
+            }
+
+
+
             Vector3 desired_velocity = ArriveBehavior.DesiredVelocityForArrival(mine, closestLeader.Position - closestLeader.Forward.normalized * followDistance, stoppingRadius);
             steer = desired_velocity - mine.Velocity;
             steer = steer.normalized * Mathf.Min(steer.magnitude, mine.activeSettings.maxForce);
