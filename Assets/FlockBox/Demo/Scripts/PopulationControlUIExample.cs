7eafe558a499dee857380f911ee8693058b6a968
mbhuet <mbhuet@gmail.com>
2020-04-26 14:00:15 -0500

    rename script

--- Assets/FlockBox/Demo/Scripts/PopulationControlUIExample.cs
+++ Assets/FlockBox/Demo/Scripts/PopulationControlUIExample.cs
@@ -5,18 +5,26 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace CloudFine{
-    public class DemoPopulationControl : MonoBehaviour
+    /// <summary>
+    /// Example of how Agent population can be modified.
+    /// Click to anywhere to spawn a new Agent.
+    /// Removed agents are hidden and cached using Kill() instead of destroyed.
+    /// New agents are pulled from cache if possible instead of creating a new object.
+    /// </summary>
+    public class PopulationControlUIExample : MonoBehaviour
     {
         public FlockBox _flockBox;
         public Agent _agent;
-
+        public int _initialPopulation = 100;
         public Text _populationText;
 
+
         private List<Agent> _spawnedAgents = new List<Agent>();
+        private List<Agent> _cachedAgents = new List<Agent>();
 
         private void Start()
         {
-            for(int i=0; i<100; i++)
+            for(int i=0; i<_initialPopulation; i++)
             {
                 AddAgent();
             }
@@ -26,10 +34,18 @@ namespace CloudFine{
         {
             if (Input.GetMouseButtonDown(0))
             {
+                //check to see if the click was over UI
                 if ((!EventSystem.current.IsPointerOverGameObject()) )
                 {
-                    Vector3 clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    AddAgent(clickPos);
+                    ClickAddAgent(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                }
+            }
+            if (Input.GetMouseButtonDown(1))
+            {
+                //check to see if the click was over UI
+                if ((!EventSystem.current.IsPointerOverGameObject()))
+                {
+                    RemoveAgent();
                 }
             }
         }
@@ -44,7 +60,16 @@ namespace CloudFine{
 
         public void AddAgent()
         {
-            Agent agent = GameObject.Instantiate<Agent>(_agent);
+            Agent agent;
+            if (_cachedAgents.Count > 0)
+            {
+                agent = _cachedAgents[0];
+                _cachedAgents.RemoveAt(0);
+            }
+            else
+            {
+                agent = GameObject.Instantiate<Agent>(_agent);
+            }
             _spawnedAgents.Add(agent);
             agent.Spawn(_flockBox);
             RefreshPopulationCount();
@@ -56,12 +81,14 @@ namespace CloudFine{
             {
                 Agent toDestroy = _spawnedAgents[0];
                 _spawnedAgents.RemoveAt(0);
-                GameObject.Destroy(toDestroy);
+                //use kill to deactivate and cache the Agent instead of destroying
+                toDestroy.Kill();
+                _cachedAgents.Add(toDestroy);
                 RefreshPopulationCount();
             }
         }
 
-        private void AddAgent(Vector3 position)
+        private void ClickAddAgent(Vector3 position)
         {
             Agent agent = GameObject.Instantiate<Agent>(_agent);
             agent.Spawn(_flockBox, position);
