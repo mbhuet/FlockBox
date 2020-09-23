a8aaefc6d4c202f5eb85f78983bbec760a3e1306
mbhuet <mbhuet@gmail.com>
2020-09-23 10:36:42 -0500

    Planetoid Demo

---
+++ Assets/FlockBox/Scripts/Core/SteeringAgents/PlanetoidSteeringAgent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CloudFine.FlockBox
+{
+    public class PlanetoidSteeringAgent : SteeringAgent
+    {
+        protected override void UpdateTransform()
+        {
+
+            ///////
+            ///The planetoid's center and radius are assumed to be the center of the FlockBox and its smallest dimension.
+            Vector3 planetoidCenter = myNeighborhood.WorldDimensions/2f;
+            float planetoidRadius = Mathf.Min(myNeighborhood.WorldDimensions.x, myNeighborhood.WorldDimensions.y, myNeighborhood.WorldDimensions.z)/2f;
+            ///////
+
+            Vector3 planetoidUp = (Position - planetoidCenter).normalized;
+            Vector3 clampedPosition = planetoidCenter + planetoidUp * planetoidRadius;
+
+            Position = clampedPosition;
+            transform.localPosition = Position;
+
+
+            //project velocity onto plane tangent to planetoid surface
+            Velocity = Vector3.ProjectOnPlane(Velocity, planetoidUp);
+
+
+            if (Velocity.magnitude > 0)
+            {
+                transform.localRotation = Quaternion.LookRotation(Velocity, planetoidUp);
+                Forward = Velocity;
+            }
+
+            else
+            {
+                Forward = transform.localRotation * Vector3.forward;
+            }
+        }
+
+    }
+}
