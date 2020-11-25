8a1da63566fd7a99f29eba69fcb6bd699476d834
mbhuet <mbhuet@gmail.com>
2020-11-25 11:56:42 -0600

    abandoning the DOTS behavior swap system

---
+++ Assets/Development/DOTSBehaviorSwap/AgentStateData.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace CloudFine.FlockBox.DOTS
+{
+    public struct AgentStateData : IComponentData
+    {
+        public int State;
+    }
+}
