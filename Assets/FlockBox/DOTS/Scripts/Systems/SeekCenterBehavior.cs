64d3591b1a08aba089277c20fa4c1c4c3e26cd0d
mbhuet <mbhuet@gmail.com>
2022-02-20 23:16:42 -0600

    Add SeekCenterBehavior

---
+++ Assets/FlockBox/DOTS/Scripts/Systems/SeekCenterBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CloudFine.FlockBox
+{
+    [System.Serializable]
+    public class SeekCenterBehavior : SteeringBehavior
+    {
+        public override void GetSteeringBehaviorVector(out Vector3 steer, SteeringAgent mine, SurroundingsContainer surroundings)
+        {
+            mine.GetSeekVector(out steer, mine.FlockBox.WorldDimensions / 2f);
+        }
+    }
+}
