0a2085c84d51eddfb4ea131eb5f078a94808fed3
mbhuet <mbhuet@gmail.com>
2022-08-28 12:20:08 -0500

    add visualization for cells

--- Assets/FlockBox/Classic/Scripts/Editor/FlockBoxEditor.cs
+++ Assets/FlockBox/Classic/Scripts/Editor/FlockBoxEditor.cs
@@ -34,6 +34,8 @@ namespace CloudFine.FlockBox
         private bool optimizationFoldout = false;
         private bool debugFoldout = false;
 
+        private Vector3[] faces;
+
         private void OnEnable()
         {
             _dimensionX = serializedObject.FindProperty("dimensions_x");
@@ -52,6 +54,16 @@ namespace CloudFine.FlockBox
             _useCellCapacity = serializedObject.FindProperty("capCellCapacity");
             _useDOTS = serializedObject.FindProperty("useDOTS");
             _worldSpace = serializedObject.FindProperty("useWorldSpace");
+
+            faces = new Vector3[]
+{
+                Vector3.left,
+                Vector3.right,
+                Vector3.up,
+                Vector3.down,
+                Vector3.forward,
+                Vector3.back
+};
         }
 
         public override void OnInspectorGUI()
@@ -145,6 +157,125 @@ namespace CloudFine.FlockBox
 
 
             serializedObject.ApplyModifiedProperties();
+        }
+
+        public void OnSceneGUI()
+        {
+            Vector3Int dimensions = new Vector3Int(_dimensionX.intValue, _dimensionY.intValue, _dimensionZ.intValue);
+            float size = _size.floatValue;
+
+            Handles.matrix = (target as FlockBox).transform.localToWorldMatrix;
+            Handles.color = Color.grey * .5f;
+
+            Camera sceneCam = SceneView.lastActiveSceneView.camera;
+            Vector3 camForward = Handles.matrix.inverse.MultiplyVector(sceneCam.transform.forward);
+            Vector3 camPos = Handles.matrix.inverse.MultiplyPoint(sceneCam.transform.position);
+            bool isOrtho = sceneCam.orthographic;
+
+            for(int f = 0; f<faces.Length; f++)
+            {
+                Vector3 faceNormal = faces[f];
+                Vector3 faceCenter = Vector3.Scale((Vector3.one + faceNormal) * .5f, (Vector3)dimensions * size);
+                Vector3 faceToCam = camPos - faceCenter;
+                bool facingCamera = isOrtho? Vector3.Dot(camForward, faceNormal) < 0 : Vector3.Dot(faceNormal, faceToCam) > 0;
+                if (facingCamera)
+                {
+                    if (faceNormal.x != 0)
+                    {
+                        int x = faceNormal.x < 0 ? 0 : dimensions.x;
+                        for(int y = 0; y<dimensions.y+1; y++)
+                        {
+                            Handles.DrawAAPolyLine(
+                                new Vector3(x, y, 0) * size,
+                                new Vector3(x, y, dimensions.z) * size
+                                );                            
+                        }
+                        for (int z = 0; z < dimensions.z + 1; z++)
+                        {
+                            Handles.DrawAAPolyLine(
+                                new Vector3(x, 0, z) * size,
+                                new Vector3(x, dimensions.y, z) * size
+                                );
+                        }
+                    }
+                    if (faceNormal.y != 0)
+                    {
+                        float y = faceNormal.y < 0 ? 0 : dimensions.y;
+                        for (int x = 0; x < dimensions.x + 1; x++)
+                        {
+                            Handles.DrawAAPolyLine(
+                                new Vector3(x, y, 0) * size,
+                                new Vector3(x, y, dimensions.z) * size
+                                );                  
+                        }
+                        for (int z = 0; z < dimensions.z + 1; z++)
+                        {
+                            Handles.DrawAAPolyLine(
+                                new Vector3(0, y, z) * size,
+                                new Vector3(dimensions.x, y, z) * size
+                                );
+                        }
+                    }                   
+                    if(faceNormal.z != 0)
+                    {
+                        float z = faceNormal.z < 0 ? 0 : dimensions.z;
+                      
+                        for (int x = 0; x < dimensions.x + 1; x++)
+                        {
+                            Handles.DrawAAPolyLine(
+                                new Vector3(x, 0, z) * size,
+                                new Vector3(x, dimensions.y, z) * size
+                                );                       
+                        }                      
+                        for (int y = 0; y < dimensions.y + 1; y++)
+                        {
+                            Handles.DrawAAPolyLine(
+                                new Vector3(0, y, z) * size,
+                                new Vector3(dimensions.x, y, z) * size
+                                );
+                        }
+                    }
+
+                }
+            }
+
+            /*
+            for (int x = 0; x < _dimensionX.intValue +1; x++) 
+            {
+                for (int y = 0; y < _dimensionY.intValue +1; y++)
+                {
+                    for (int z = 0; z < _dimensionZ.intValue + 1; z++)
+                    {
+                        Handles.DrawAAPolyLine(new Vector3[]
+                        {
+                        new Vector3(x * size,   0,                      0),
+                        new Vector3(x * size,   dimensions.y * size,    0),
+                        new Vector3(x * size,   dimensions.y * size,    dimensions.z * size),
+                        new Vector3(x * size,   0,                      dimensions.z * size),
+                        new Vector3(x * size,   0,                      0),
+                        });
+
+                        Handles.DrawAAPolyLine(new Vector3[]
+                        {
+                        new Vector3(0,                      y * size,   0),
+                        new Vector3(dimensions.x * size,    y * size,   0),
+                        new Vector3(dimensions.x * size,    y * size,   dimensions.z * size),
+                        new Vector3(0,                      y * size,   dimensions.z * size),
+                        new Vector3(0,                      y * size,   0),
+                        }); ;
+
+                        Handles.DrawAAPolyLine(new Vector3[]
+                        {
+                        new Vector3(0,                      0,                      z * size),
+                        new Vector3(dimensions.x * size,    0,                      z * size),
+                        new Vector3(dimensions.x * size,    dimensions.y * size,    z * size),
+                        new Vector3(0,                      dimensions.y * size,    z * size),
+                        new Vector3(0,                      0,                      z * size),
+                        });
+                    }
+                }
+            }
+            */
         }
     }
 }
