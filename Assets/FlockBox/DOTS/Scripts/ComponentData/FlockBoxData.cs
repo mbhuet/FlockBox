37840693a1853b4b9c018e76e46596897d23ac3a
mbhuet <mbhuet@gmail.com>
2023-04-30 17:03:39 -0500

    empty flock data

---
+++ Assets/FlockBox/DOTS/Scripts/ComponentData/FlockBoxData.cs
@@ -0,0 +1,13 @@
+#if FLOCKBOX_DOTS
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace CloudFine.FlockBox.DOTS
+{
+    public struct FlockBoxData : IComponentData
+    {
+        
+    }
+}
+#endif
